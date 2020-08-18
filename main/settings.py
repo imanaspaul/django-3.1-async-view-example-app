c0d0ae1d0aba2cb46714ab8ab448abc76b889a63
Manas <imanaspaul@gmail.com>
2020-08-18 19:58:15 +0530

    Updataed

--- main/settings.py
+++ main/settings.py
@@ -121,3 +121,8 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
