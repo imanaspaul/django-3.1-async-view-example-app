3d345f0e8dfa8153e23938ae3469584748b48336
Manas <imanaspaul@gmail.com>
2020-08-18 19:54:06 +0530

    runtime setup

--- main/settings.py
+++ main/settings.py
@@ -112,7 +112,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles') 
+
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
